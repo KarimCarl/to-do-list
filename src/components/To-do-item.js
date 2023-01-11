744215d6784fb95271455e31e2ca1406724aa9ae
KarimCarl <cndungu109@gmail.com>
2023-01-11 14:54:47 +0300

    Modified items in the file

--- src/components/To-do-item.js
+++ src/components/To-do-item.js
@@ -1,8 +1,8 @@
 function ToDoItem () {
     return (
         <div> 
-            <h3>Item</h3>
-            <p>Lorem ipsum</p>
+            <h3>React</h3>
+            <p>Lear react</p>
         </div>
     );
 }
