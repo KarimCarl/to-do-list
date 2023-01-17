fe500f2c1df79694e8adf1e84b6c6d2bdda47a9a
KarimCarl <cndungu109@gmail.com>
2023-01-17 12:33:14 +0300

    Added routing

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,37 +3,42 @@ import NavBar from "../component/NavBar";
 import ToDoList from "../component/ToDoList";
 import Footer from "../component/Footer";
 import AddTodo from "../component/AddTodo";
+
 function Home(){
     const [todos, setTodos] = useState([
         {
             "id": 1,
             "title":" learn react",
-            "Description":"introduction  to react "
+            "Description":"introduction  to react ",
+            "status": false
         },
         {
             "id": 2,
             "title": "Go to the market",
-            "Description": "Buy some groceries and fruits for my granny"
+            "Description": "Buy some groceries and fruits for my granny",
+            "status": false
         },
         {
-            "id": 2,
+            "id": 3,
             "title": "Go to the gym",
-            "Description": "Do a 2 hr body workout"
+            "Description": "Do a 2 hr body workout",
+            "status": false
         }
-    ])
+    ]);
+    
     return(
-       <div>
-        <div>
-            <NavBar />
-        </div>
-        <div>
-            <AddTodo />
-            <ToDoList todos={todos} />
-        </div>
-        <div>
-            <Footer />
-        </div>
-        </div>
+       <>
+            <div>
+                <NavBar />
+            </div>
+            <div>
+                <AddTodo />
+                <ToDoList todos={todos} />
+            </div>
+            <div>
+                <Footer />
+            </div>
+        </>
     );
 }
 export default Home;
