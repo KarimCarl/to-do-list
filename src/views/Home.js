4c7032b2274c374f081980bdb2916c557b4c8c6e
KarimCarl <cndungu109@gmail.com>
2023-01-17 09:53:18 +0300

    Deleted unwanted code from original app

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,20 +1,39 @@
-import ToDoList from "../components/To-do-list"
-import NavBar from "../components/NavBar";
-
+import React, { useState } from "react";
+import NavBar from "../component/NavBar";
+import ToDoList from "../component/ToDoList";
+import Footer from "../component/Footer";
+import AddTodo from "../component/AddTodo";
 function Home(){
-    return (
-        <div> 
-            <div>
-                <NavBar />
-            </div>
-
-            <div> 
-                <ToDoList /> 
-            </div> 
-        </div>   
-            
-
+    const [todos, setTodos] = useState([
+        {
+            "id": 1,
+            "title":" learn react",
+            "Description":"introduction  to react "
+        },
+        {
+            "id": 2,
+            "title": "Go to the market",
+            "Description": "Buy some groceries and fruits for my granny"
+        },
+        {
+            "id": 2,
+            "title": "Go to the gym",
+            "Description": "Do a 2 hr body workout"
+        }
+    ])
+    return(
+       <div>
+        <div>
+            <NavBar />
+        </div>
+        <div>
+            <AddTodo />
+            <ToDoList todos={todos} />
+        </div>
+        <div>
+            <Footer />
+        </div>
+        </div>
     );
 }
-
 export default Home;
