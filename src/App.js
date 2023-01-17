d216b42cdb716401daeffc39849a05f7b8e260ea
KarimCarl <cndungu109@gmail.com>
2023-01-17 12:33:33 +0300

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Component, startTransition } from 'react';
 import Home from './views/Home';
 
 function App() {
-  return (
-    <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
->>>>>>> fe69cb6f686f86345d036be22a660beafe70b2ca
-    </div>
+  return(
+    <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/analytics"} element={<ToDoAnalytics />} />
+        <Route path={"/"} element={<To-do-item />} />
+    </Routes>
   );
 }
 
-export default App;
+export default App; 
